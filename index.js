0ebe52b621bdeb5cc37ded735c2472325d726c07
semil338 <semilkhedawala@gmail.com>
2023-08-02 14:57:34 +0530

    handle invalid initial directions

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function applyCommands(commands, startingPosition, initialDirection) {
     const directions = ["N", "E", "S", "W", "U", "D"];
     const directionIndex = { N: 0, E: 1, S: 2, W: 3, U: 4, D: 5 };
 
-   
+    // Return if Invalid initialDirection
+    if (!directionIndex.hasOwnProperty(initialDirection)) {
+        console.error(`Invalid initial direction "${initialDirection}".`);
+        return;
+    }
 
   
 }
