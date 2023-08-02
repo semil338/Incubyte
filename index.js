f89ca97a07c1fae1e9caff9252140e293d87cdfd
semil338 <semilkhedawala@gmail.com>
2023-08-02 14:57:03 +0530

    initializing all directions

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+function applyCommands(commands, startingPosition, initialDirection) {
+
+    // Initialize All Directions
+    const directions = ["N", "E", "S", "W", "U", "D"];
+    const directionIndex = { N: 0, E: 1, S: 2, W: 3, U: 4, D: 5 };
+
+   
+
+  
+}
+
+
 // Commands
 const commands = ["f", "r", "u", "b", "l"];
 const startingPosition = [0, 0, 0];
 const initialDirection = "N";
+
