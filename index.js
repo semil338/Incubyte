28eee0a77edd819902a0496bcd79d75a59f735db
semil338 <semilkhedawala@gmail.com>
2023-08-02 15:06:01 +0530

    loop every command and update

--- index.js
+++ index.js
@@ -10,7 +10,57 @@ function applyCommands(commands, startingPosition, initialDirection) {
         return;
     }
 
-  
+    // Initialize the current position and direction
+    let currentPosition = [...startingPosition];
+    let currentDirectionIndex = directionIndex[initialDirection];
+
+    // Loop through every command and update the values
+    for (const command of commands) {
+        console.log(currentPosition, directions[currentDirectionIndex]);
+        switch (command) {
+            case "f":
+                // Move forward in the x-direction (East)
+                if (currentDirectionIndex === 1) currentPosition[0]++; 
+                // Move forward in the y-direction (North)
+                if (currentDirectionIndex === 0) currentPosition[1]++;
+                // Move upward in the z-direction (Up Down)
+                if (currentDirectionIndex === 4) currentPosition[2]++;
+                break;
+            case "b":
+                // Move backward in the x-direction (West)
+                if (currentDirectionIndex === 1) currentPosition[0]--; 
+                // Move backward in the y-direction (South)
+                if (currentDirectionIndex === 0) currentPosition[1]--;
+                // Move downward in the z-direction
+                if (currentDirectionIndex === 4) currentPosition[2]--;
+                break;
+            case "r":
+                // Turn right (N -> E -> S -> W -> N)
+                currentDirectionIndex = (currentDirectionIndex + 1) % 4; 
+                break;
+            case "l":
+                // Turn left (N -> W -> S -> E -> N)
+                currentDirectionIndex = (currentDirectionIndex - 1 + 4) % 4; 
+                break;
+            case "u":
+                // Change to "Up" direction
+                currentDirectionIndex = 4; 
+                break;
+            case "d":
+                // Change to "Down" direction
+                currentDirectionIndex = 5; 
+                break;
+            default:
+                // Handle invalid command
+                console.error(`Invalid command "${command}"`);
+        }
+    }
+
+    // return posiition and direction
+    return {
+        finalPosition: currentPosition,
+        finalDirection: directions[currentDirectionIndex]
+    };
 }
 
 
@@ -18,4 +68,5 @@ function applyCommands(commands, startingPosition, initialDirection) {
 const commands = ["f", "r", "u", "b", "l"];
 const startingPosition = [0, 0, 0];
 const initialDirection = "N";
+
 
