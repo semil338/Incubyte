85342d8ce8d647d34cf5c06f435db1d84babdc25
Semil Khedawala <semilkhedawala@gmail>
2023-08-02 20:35:11 +0530

    updating test cases

--- program.test.js
+++ program.test.js
@@ -1,65 +1,127 @@
 const calculatePoints = require("./calculatePoints");
 
-describe('calculatePoints function', () => {
+describe("calculatePoints function", () => {
+  test("Initial case - 1", () => {
+    const commands = ["f", "r"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "N";
 
-    test('Initial case - 1', () => {
-        const commands = ["f", "r", "u", "b", "l"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "N";
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 1, 0]);
+    expect(result.finalDirection).toBe("E");
+  });
+  test("Initial case - 2", () => {
+    const commands = ["f", "r", "u"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "N";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([0, 1, -1]);
-        expect(result.finalDirection).toBe("W");
-    });
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 1, 0]);
+    expect(result.finalDirection).toBe("U");
+  });
+  test("Initial case - 3", () => {
+    const commands = ["f", "r", "u", "b"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "N";
 
-    test('Should handle invalid commands gracefully and skip them - 2', () => {
-        const commands = ["f", "r", "u", "b", "l","x"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "N";
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 1, -1]);
+    expect(result.finalDirection).toBe("U");
+  });
+  test("Initial case - 4", () => {
+    const commands = ["f", "r", "u", "b", "l"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "N";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([0, 1, -1]);
-        expect(result.finalDirection).toBe("W");
-    });
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 1, -1]);
+    expect(result.finalDirection).toBe("W");
+  });
 
-    test('Custom value case - 1', () => {
-        const commands = ["f", "l", "b", "u", "f", "r"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "S";
+  test("Should handle invalid commands gracefully and skip them - 2", () => {
+    const commands = ["f", "r", "u", "b", "l", "x"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "N";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([-1, 1, 1]);
-        expect(result.finalDirection).toBe("E");
-    }); 
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 1, -1]);
+    expect(result.finalDirection).toBe("W");
+  });
 
-    test('Custom value case - 2', () => {
-        const commands = ["b", "u", "b", "l", "f"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "E";
+  test("Custom value case - 1", () => {
+    const commands = ["f", "l", "b", "u", "f", "r"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "S";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([0, 0, -1]);
-        expect(result.finalDirection).toBe("W");
-    }); 
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    console.log(result);
+    expect(result.finalPosition).toStrictEqual([-1, 1, 1]);
+    expect(result.finalDirection).toBe("S");
+  });
 
-    test('Custom value case - 3', () => {
-       const commands = ["f", "r","l", "b", "u", "f", "r"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "W";
+  test("Custom value case - 2", () => {
+    const commands = ["b", "u", "b", "l", "f"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "E";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([0,0,1]);
-        expect(result.finalDirection).toBe("E");
-    });
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 0, -1]);
+    expect(result.finalDirection).toBe("W");
+  });
 
-    test('Custom directions case ', () => {
-       const commands = ["u", "r"];
-        const startingPosition = [0, 0, 0];
-        const initialDirection = "W";
+  test("Custom value case - 3", () => {
+    const commands = ["f", "r", "l", "b", "u", "f", "r"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "W";
 
-        const result = calculatePoints(commands, startingPosition, initialDirection);
-        expect(result.finalPosition).toStrictEqual([0,0,0]);
-        expect(result.finalDirection).toBe("N");
-    });
-    
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 0, 1]);
+    expect(result.finalDirection).toBe("N");
+  });
+
+  test("Custom directions case ", () => {
+    const commands = ["u", "r"];
+    const startingPosition = [0, 0, 0];
+    const initialDirection = "W";
+
+    const result = calculatePoints(
+      commands,
+      startingPosition,
+      initialDirection
+    );
+    expect(result.finalPosition).toStrictEqual([0, 0, 0]);
+    expect(result.finalDirection).toBe("N");
+  });
 });
